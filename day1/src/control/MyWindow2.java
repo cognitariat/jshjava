0579bf5605f7d12b8a23d749f989ecfa5627e84e
cognitariat <sshine1024@hotmail.com>
2019-01-05 14:18:33 +0900

    Add files via upload

---
+++ day1/src/control/MyWindow2.java
@@ -0,0 +1,20 @@
+package control;
+
+import javax.swing.JFrame;
+
+public class MyWindow2 {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize(1000, 1000);
+		f.setTitle("나의 갤러거");
+		f.setVisible(true);
+	}
+
+}
+
+
+
+
+
+
